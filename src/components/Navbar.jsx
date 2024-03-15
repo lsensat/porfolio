cf01ec6fad6ef9c168cfc5edcc7fbdcd407ece76
luis sensat <luis.sensat@gmail.com>
2024-03-15 07:46:13 +0100

    starting portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <ul id="menu" className="bg-white">
+      <li>Me</li>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Contacts</li>
+    </ul>
+  )
+}
